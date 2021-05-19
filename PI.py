5f50a8d82c27893dedb8e8bb8c04789d98192088
Joshua Jerred <jdjerred@gmail.com>
2021-05-19 09:35:06 -0600

    Created a uniform number format for the system data. Decimal accuracy isn't needed and this will save time when sending data.

--- PI.py
+++ PI.py
@@ -15,13 +15,13 @@ class data:
         data = open('/sys/class/thermal/thermal_zone0/temp')
         raw_cputemp = int(data.read())
         data.close()
-        self.cpu_temp = raw_cputemp / 1000
+        self.cpu_temp = int(raw_cputemp / 1000)
 
         # CPU usage
-        self.cpu_usage = float(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip())
+        self.cpu_usage = int(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip())
 
         # RAM usage
-        self.ram_usage = round(float(os.popen("free | awk '/Mem/ {print $3/$2}'").readline().strip()) * 100, 1)
+        self.ram_usage = int(os.popen("free | awk '/Mem/ {print $3/$2}'").readline().strip() * 100)
 
         # Disk info
         self.disk_usage = int((os.popen("df -m | awk '/root/ {print $5}'").readline().strip())[0:-1])

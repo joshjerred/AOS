c781a1c2acbaff10bd221f8bfa74cf078cb6129e
Joshua Jerred <jdjerred@gmail.com>
2021-05-25 08:41:21 -0600

    logging added to bmp180.py

--- bmp180.py
+++ bmp180.py
@@ -1,13 +1,29 @@
 import smbus
 import time
 from ctypes import c_short
+import logging
 
+# logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+
+file_handler = logging.FileHandler('./logs/sensor_logs/bmp180.log')
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+# class to read the bmp180 sensor
 class data:
   def __init__(self):
+    logger.debug('starting init()')
     self.DEVICE = 0x77 # Default device I2C address
-    #bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
-    self.bus = smbus.SMBus(1) # Rev 2 Pi uses 1
+    self.bus = smbus.SMBus(1)
 
     self.raw_data = ""
     self.raw_press = 0
@@ -18,26 +34,31 @@ class data:
     self.chip_version = ""
 
 
-  def __convertToString(self, data):
-    # Simple function to convert binary data into a string
-    return str((data[1] + (256 * data[0])) / 1.2)
+  # def __convertToString(self, data):
+  #   # Simple function to convert binary data into a string
+  #   return str((data[1] + (256 * data[0])) / 1.2)
 
   def __getShort(self, data, index):
+    logger.debug('starting __getShort()')
     # return two bytes from data as a signed 16-bit value
     return c_short((data[index] << 8) + data[index + 1]).value
 
   def __getUshort(self, data, index):
+    logger.debug('starting __getUshort')
     # return two bytes from data as an unsigned 16-bit value
     return (data[index] << 8) + data[index + 1]
 
   def readBmp180Id(self):
+    logger.debug('starting readBmp180Id()')
     addr=self.DEVICE
     # Chip ID Register Address
     REG_ID     = 0xD0
     (self.chip_id, chip_version) = self.bus.read_i2c_block_data(addr, REG_ID, 2)
+    logger.debug('chip id is: ' + str(self.chip_id))
     return (self.chip_id, chip_version)
 
   def pull(self):
+    logger.debug('starting pull()')
     addr=self.DEVICE
     # Register Addresses
     REG_CALIB  = 0xAA
@@ -112,15 +133,19 @@ class data:
     self.raw_backup_temp = temperature
     self.backup_temp = temperature / 10
     self.pressm = pressure/100.0
+    logging.debug('raw pressure: ' + str(self.raw_press), 'raw temperature: ' + str(self.raw_backup_temp))
 
   def read_backup_temp(self):
+    logger.debug('starting read_back_temp()')
     return self.backup_temp
 
   def read_raw(self):
+    logger.debug('starting read_raw()')
     "raw_data, crc?, raw_press, pressmbar, backup_temp, raw_backup_temp"
     return [self.raw_data, self.raw_press, self.raw_backup_temp]
 
   def read(self):
+    logger.debug('starting read()')
     """returns human readable format for the pressure"""
     return self.pressm
 
